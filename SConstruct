c54fb288a9c2ecc3a50b1e86279708641feb431b
Nicklas Behrndtz <nickl4sxd1@gmail.com>
2025-06-16 09:51:40 +0200

    Complete initial SCons configuration

---
+++ SConstruct
@@ -0,0 +1,5 @@
+# Main Build
+# SConscript("main.scons", variant_dir = 'build', duplicate = 0)
+
+# Example Build
+SConscript("imgui/example.scons", variant_dir = 'build', duplicate = 0)
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+
+int main()
+{
+    // // Setup Dear ImGui context
+    // IMGUI_CHECKVERSION(); // WHAT DOES THIS DO???
+    // ImGui::CreateContext();
+    // ImGuiIO& io = ImGui::GetIO();
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    
+    // ImGui_ImplWin32_Init();
+    // ImGui_ImplDX11_Init();
+    
+    return 0;
+}
