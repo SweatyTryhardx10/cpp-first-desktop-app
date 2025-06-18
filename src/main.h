82b0f9a8422b0b7e5f451b5845a973e7002a8570
Nicklas Behrndtz <nickl4sxd1@gmail.com>
2025-06-18 08:38:04 +0200

    Initial setup of window desktop rendering

---
+++ src/main.h
@@ -0,0 +1,12 @@
+#include "imgui.h"
+#include <d3d11.h>
+
+#pragma once
+
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+void MainLoop();
